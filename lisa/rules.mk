9881d1fb58749e65c84f5ad14052f26c67474e63
nonene-n <96224361+nonene-n@users.noreply.github.com>
2021-12-16 13:15:19 +0700

    Add files via upload

---
+++ lisa/rules.mk
@@ -0,0 +1,5 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+SPLIT_KEYBOARD = yes
+NKRO_ENABLE = yes
